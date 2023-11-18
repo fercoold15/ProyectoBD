e634c5814c1e298f5c80fc8ec1927fca50db2716
Fercoold <fernandomaldonado751@gmail.com>
2023-11-17 20:22:22 -0600

    Clase 17/11/2023

--- src/main/java/proyecto/Main.java
+++ src/main/java/proyecto/Main.java
@@ -2,16 +2,48 @@ package proyecto;
 
 import proyecto.modelos.Producto;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Query query = new Query();
-        Producto producto = new Producto();
-        producto=query.getProducto("Aceite");
-        System.out.println(producto.getNombre()+" Descripcion: "+producto.getDescripcion());
-        ;
-//        for (Producto productos :
-//            query.getProductos().values()) {
-//            System.out.println(productos.getNombre()+" "+ productos.getDescripcion());
-//        }
+        int opcion=0;
+        do {
+            Scanner opciones = new Scanner(System.in);
+            opcion=opciones.nextInt();
+            switch (opcion){
+                case 1:
+                    query.getProductos();
+                    break;
+                case 2:
+                    query.getProducto("");
+                    break;
+                case 3:
+                    Scanner scanner = new Scanner(System.in);
+                    Producto producto = new Producto();
+                    System.out.println("Ingresa el nombre del producto: ");
+                    producto.setNombre(scanner.nextLine());
+                    System.out.println("Ingresa la descripcion del producto: ");
+                    producto.setDescripcion(scanner.nextLine());
+                    query.insertProducto(producto);
+                    break;
+                case 4:
+                    Scanner actualizacion = new Scanner(System.in);
+                    Producto productoActualizado = new Producto();
+                    System.out.println("Ingresa el nombre del producto: ");
+                    productoActualizado.setNombre(actualizacion.nextLine());
+                    System.out.println("Ingresa la descripcion del producto: ");
+                    productoActualizado.setDescripcion(actualizacion.nextLine());
+                    System.out.println("Ingresa el id del producto:");
+                    int id=actualizacion.nextInt();
+                    query.updateProducto(productoActualizado,id);
+                    break;
+                case 5:
+                    Scanner delete = new Scanner(System.in);
+                    System.out.println("Ingresa el id del producto:");
+                    int idAEliminar=delete.nextInt();
+                    query.deleteProducto(idAEliminar);
+            }
+        }while (opcion!=0);
     }
 }
--- src/main/java/proyecto/Query.java
+++ src/main/java/proyecto/Query.java
@@ -2,59 +2,113 @@ package proyecto;
 
 import proyecto.modelos.Producto;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Query {
-    String url="jdbc:mysql://127.0.0.1:3306/IF1023";
-    String usuario="root";
-    String contrasenia="admin";
-    Conexion conexion = new Conexion(url,usuario,contrasenia);
-    public Map<String, Producto> getProductos(){
-        String getProductos="SELECT * FROM Productos";
+    String url = "jdbc:mysql://127.0.0.1:3306/IF1023";
+    String usuario = "root";
+    String contrasenia = "admin";
+
+
+    public Map<String, Producto> getProductos() {
+        Conexion conexion = new Conexion(url, usuario, contrasenia);
+        String getProductos = "SELECT * FROM Productos";
         Statement statement = null;
-        ResultSet resultSet =null;
+        ResultSet resultSet = null;
         Map<String, Producto> listaProductos = new HashMap<>();
-        try{
+        try {
             Connection connection = conexion.getConnection();
             statement = connection.prepareStatement(getProductos);
-            resultSet=statement.executeQuery(getProductos);
-            while (resultSet.next()){
+            resultSet = statement.executeQuery(getProductos);
+            while (resultSet.next()) {
                 Producto productos = new Producto();
                 productos.setNombre(resultSet.getString(2));
                 productos.setDescripcion(resultSet.getString(3));
                 listaProductos.put(productos.getNombre(), productos);
             }
-        }catch (Exception exception){
+        } catch (Exception exception) {
             exception.printStackTrace();
-        }finally {
+        } finally {
             conexion.cerrarConexion();
         }
-       return  listaProductos;
+        return listaProductos;
     }
-    public Producto getProducto(String nombreProducto){
+
+    public Producto getProducto(String nombreProducto) {
+        Conexion conexion = new Conexion(url, usuario, contrasenia);
         Producto producto = new Producto();
-        String getProducto="SELECT * FROM Productos WHERE NOMBRE_PRODUCTO = ?";
-        ResultSet resultSet =null;
-        PreparedStatement pstm=null;
-        try{
+        String getProducto = "SELECT * FROM Productos WHERE NOMBRE_PRODUCTO = ?";
+        ResultSet resultSet = null;
+        PreparedStatement pstm = null;
+        try {
             Connection connection = conexion.getConnection();
-            pstm=connection.prepareStatement(getProducto);
-            pstm.setString(1,nombreProducto);
-            resultSet=pstm.executeQuery();
-            while (resultSet.next()){
-              producto.setNombre(resultSet.getString(2));
-              producto.setDescripcion(resultSet.getString(3));
+            pstm = connection.prepareStatement(getProducto);
+            pstm.setString(1, nombreProducto);
+            resultSet = pstm.executeQuery();
+            while (resultSet.next()) {
+                producto.setNombre(resultSet.getString(2));
+                producto.setDescripcion(resultSet.getString(3));
             }
-        }catch (Exception exception){
+        } catch (Exception exception) {
             exception.printStackTrace();
-        }finally {
+        } finally {
             conexion.cerrarConexion();
         }
         return producto;
     }
+
+    public void insertProducto(Producto producto) {
+        Conexion conexion = new Conexion(url, usuario, contrasenia);
+        String insertProducto = "INSERT INTO Productos (NOMBRE_PRODUCTO,DESCRIPCION_PRODUCTO) values (?,?)";
+        PreparedStatement pstm = null;
+        try {
+            Connection connection = conexion.getConnection();
+            pstm = connection.prepareStatement(insertProducto);
+            pstm.setString(1, producto.getNombre());
+            pstm.setString(2, producto.getDescripcion());
+            pstm.executeUpdate();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }finally {
+            conexion.cerrarConexion();
+        }
+    }
+
+    public void updateProducto(Producto producto,int id) {
+        Conexion conexion = new Conexion(url, usuario, contrasenia);
+        String updateProducto = "UPDATE Productos SET NOMBRE_PRODUCTO=?,DESCRIPCION_PRODUCTO=? WHERE ID_PRODUCTO=?";
+        PreparedStatement pstm = null;
+        try {
+            Connection connection = conexion.getConnection();
+            pstm = connection.prepareStatement(updateProducto);
+            pstm.setString(1, producto.getNombre());
+            pstm.setString(2, producto.getDescripcion());
+            pstm.setInt(3,id);
+            pstm.executeUpdate();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }finally {
+            conexion.cerrarConexion();
+        }
+    }
+
+    public void deleteProducto(int id){
+        Conexion conexion = new Conexion(url, usuario, contrasenia);
+        String deleteProducto = "DELETE FROM Productos WHERE ID_PRODUCTO=?";
+        PreparedStatement pstm = null;
+        try {
+            Connection connection = conexion.getConnection();
+            pstm = connection.prepareStatement(deleteProducto);
+            pstm.setInt(1,id);
+            pstm.executeUpdate();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }finally {
+            conexion.cerrarConexion();
+        }
+    }
+
+
 }
